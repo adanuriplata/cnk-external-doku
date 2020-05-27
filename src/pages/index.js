f0270b3ef5c9364b881ed27ca38f10aed928f70c
FuzzJS <adanuriplata@gmail.com>
2020-05-27 12:39:37 -0500

    primer cambio

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>Encuentra respuestas a tus dudas</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
@@ -18,7 +18,7 @@ const features = [
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>Ayuda Conektica</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
